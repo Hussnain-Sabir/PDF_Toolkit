6502ab0bd23c4a13134cfab690d29189e9877d68
hussnain-sabir <hussnain.sabir.dev@gmail.com>
2026-08-02 01:20:12 +0500

    Optimization for linux and Mac

--- Pdf_Toolkit.py
+++ Pdf_Toolkit.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3       # this is for linux and mac only to access the script from terminal directly | reomove if want to run in code editor
 from pathlib import Path
 import shutil
 from PIL import Image
