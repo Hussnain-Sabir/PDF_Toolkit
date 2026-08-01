c80c52efd9cc8e2650b8e5c58b6287b4911d8b62
hussnain-sabir <hussnain.sabir.dev@gmail.com>
2026-08-01 20:23:02 +0500

    Added IMG & PDF merger

--- Pdf_Toolkit.py
+++ Pdf_Toolkit.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from PIL import Image
-from pypdf import PdfWriter
+from pypdf import PdfReader,PdfWriter
 
 path=Path.home() /  "Testing_Project"            
 # path=input("Enter the folder path: ")
@@ -9,7 +9,7 @@ path=Path.home() /  "Testing_Project"
 
 
 def image_to_PDF(path):
-    valid_extension=(".jpg",".jpeg",".png",".webp")
+    valid_extensions=(".jpg",".jpeg",".png",".webp")
 
     print("Converting into PDFs ...................")
     Target_directory= path / "IMG_to_PDF"
@@ -19,7 +19,7 @@ def image_to_PDF(path):
     
     for item in path.iterdir():
         if item.is_file():
-            if item.suffix.lower() in valid_extension:
+            if item.suffix.lower() in valid_extensions:
                 with Image.open(item) as img:
                     img=img.convert("RGB")
                     convert.append(img)
@@ -35,7 +35,7 @@ def image_to_PDF(path):
         convert[0].save(Output_path, save_all=True, append_images=convert[1:])
         print(f"Conversion Completed -> Saved to: {Target_directory.name} Folder....")
     else:
-        print("No valid images found for conversion")
+        print("No valid images found for conversion!")
 
 
 
@@ -70,14 +70,59 @@ def PDFs_Merger(path):
         print(f"PDFs Merged -> Saved to: {Target_directory.name} Folder....")
                 
     else:
-        print("No valid PDFs found for conversion")
+        print("No valid PDFs found for conversion!")
 
 
 
+
+def IMG_merge_PDF(path):
+    print("Merging Images & PDFs ...................")
+
+    valid_extensions=(".jpg",".jpeg",".png",".webp")
+
+    Target_Directory= path / "Merged_All"
+    Target_Directory.mkdir(exist_ok=True)
+
+    convert=[]
+
+    for item in path.iterdir():
+        if item.is_file():
+            if item.suffix.lower() in (valid_extensions + (".pdf",)):
+                convert.append(item)
+
+    if convert:
+        convert= sorted(convert)
+        hidden_directory= Target_Directory / ".temp_pdfs"
+        hidden_directory.mkdir(exist_ok=True)
+
+        merger= PdfWriter()
+
+        for item in convert:
+            if not item.suffix.lower() in valid_extensions:
+                merger.append(item)
+            else:
+                with Image.open(item) as img:
+                    img=img.convert("RGB")
+                    temp_path=hidden_directory / "temp_pdf.pdf"
+                    img.save(temp_path)
+                    merger.append(temp_path)
+
+        Output_path= Target_Directory / "Merged_All.pdf"
+        i=1
+        while Output_path.exists():
+            Output_path= Target_Directory / f"Merged_All_{i}.pdf"
+            i+=1
+
+        merger.write(Output_path)
+        merger.close()
+        print(f"IMGs & PDFs are Merged -> Saved to: {Target_Directory.name} Folder....")
+    else:
+        print("No valid IMGs & PDFs found for conversion!")
+        
 
 
 
 if path.exists():
-    PDFs_Merger(path)
+    IMG_merge_PDF(path)
 else:
     print(f"Path does not exits!: {path}")
